ff94fe6469e5b7c37734c0d06dba8e104f6ad582
HernanC95 <cavalieri.h95@gmail.com>
2022-11-01 11:59:51 -0300

    update1

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Cavalieri Hernan
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <div className='card black'>
+          <h2 className='titulo white flex j-center a-center'>IGNACIO BORRAZ</h2>
+          <img src='./img/sujeto1.png' alt='sujeto1' className='photo' />
+          <p className='gray flex j-center a-center'>ocultar info</p>
+          <div className='edad-fecha'>
+            <p className='datos white r25 edad'>Edad: 32 años</p>
+            <p className='datos white l25 fecha'>Fecha: 9/7/1990</p>
+          </div>
+          <div className='datos white flex a-center'>
+            <p>Mail: ignacioborraz@hotmail.com</p>
+          </div>
+          <div className='datos white flex a-center'>
+            <p>Comidas: lasaña y hamburguesa</p>
+          </div>
+          <div className='datos white flex a-center'>
+            <p>Hobbies: leer manga y bailar</p>
+          </div>
+          <div className='flex j-center a-center'>
+            <div className='gray flecha r25 flex j-center a-center'> anterior </div>
+            <div className='gray flecha l25 flex j-center a-center'> siguiente </div>
+          </div>
+        </div>
+      )
+    }
 
 export default App;
